83c8c818b5452c7688c0ad8bd7d4e757e0ac7ad6
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-20 14:20:14 +0000

    Add flickity carousel to show new arrival products on home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    
+    products = Product.objects.order_by('-date_added').all()[:8]
+   
+    template = 'home/index.html'
+    context = {
+        'products': products,
+        
+    }
+    return render(request, template, context)
