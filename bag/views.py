083b7ff31a053cf805a06a9c919e4eb9a2cd3b8f
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-24 18:12:49 +0000

    Add toast templates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
 from products.models import Product
 
 
@@ -38,6 +40,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
             
     request.session['bag'] = bag
     return redirect(redirect_url)
