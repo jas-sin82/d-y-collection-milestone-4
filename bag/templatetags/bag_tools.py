c245eaa5d496b7c3efe31188354c66581216c334
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-25 13:17:47 +0000

    Add calcc function in bag tools and add space between checout form inputs

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+# Sub total calculation for sale price
+@register.filter(name='calc_subtotal_offer')
+def calc_subtotal_offer(sale_price, quantity):
+    return sale_price * quantity
