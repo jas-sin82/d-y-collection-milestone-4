6916298e31644244a2628e5386ac646193beaaeb
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-27 21:59:14 +0000

    Tested add Product Functionality

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category,Brand
 
 
 class ProductForm(forms.ModelForm):
@@ -11,9 +11,15 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+        brands = Brand.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names_brand = [(b.id, b.get_friendly_name()) for b in brands]
 
     
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+        self.fields['brand'].choices = friendly_names_brand
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0 form-fields'
