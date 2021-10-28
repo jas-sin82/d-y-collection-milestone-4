0ffb9dd985cc29ab2976516806336291be5f7f5c
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-28 10:41:46 +0000

    tested product add and delete and complete the image uplpload part

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Category,Brand
+from .models import Product, Category, Brand
 
 
 class ProductForm(forms.ModelForm):
