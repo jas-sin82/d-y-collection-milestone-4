bc67df3e9f13e66a1770236da6de619fad2caa99
jas-sin82 <jasvir198@yahoo.co.in>
2021-10-29 08:46:48 +0000

    deploying a app to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn david_yenny_collection.wsgi:application
--- david_yenny_collection/settings.py
+++ david_yenny_collection/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g-2d42dqp3jsc)7m632d41_*vd7bg3#$@c9f=&$(bbl9b5*+ev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['d-y-collection-milestone-4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'david_yenny_collection.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
